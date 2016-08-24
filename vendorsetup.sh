500711187fcc11409e59514af0009e459e79cb19
Leonardo Henrique <leonardohenriquesouza01@hotmail.com>
2016-08-24 14:05:16 -0300

    Bringup AOSP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,10 @@ add_lunch_combo aokp_T00F-user
 add_lunch_combo aokp_T00F-userdebug
 add_lunch_combo aokp_T00F-eng
 
+add_lunch_combo aosp_T00F-user
+add_lunch_combo aosp_T00F-userdebug
+add_lunch_combo aosp_T00F-eng
+
 add_lunch_combo cm_T00F-user
 add_lunch_combo cm_T00F-userdebug
 add_lunch_combo cm_T00F-eng
