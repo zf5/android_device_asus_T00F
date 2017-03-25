1399a2ea9367f2da5c6f04206b2c242ee9c20c60
Flex1911 <dedsa2002@gmail.com>
2017-03-25 10:55:22 +0300

    T00F: don't use full recovery image
    Change-Id: If8bb3b126a2e62243d975d4ff1d83f80a4024b08

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,6 @@ TARGET_DROIDBOOT_LIBS := libintel_droidboot
 TARGET_RELEASETOOL_MAKE_RECOVERY_PATCH_SCRIPT := ./device/asus/Z00D/make_recovery_patch
 
 # OTA Packaging / Bootimg creation
-BOARD_USES_FULL_RECOVERY_IMAGE := true
 BOARD_CUSTOM_MKBOOTIMG := pack_intel
 BOARD_CUSTOM_BOOTIMG_MK := device/asus/T00F/mkbootimg.mk
 DEVICE_BASE_BOOT_IMAGE := device/asus/T00F/base_images/boot.img
