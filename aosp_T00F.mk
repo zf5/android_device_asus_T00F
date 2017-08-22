9fda389bcd7ef532419367160f353d7613bbb27d
tank0412 <romabukhtiarov@gmail.com>
2017-08-22 05:09:07 +0300

    T00F: aosp bring up

--- aosp_T00F.mk
+++ aosp_T00F.mk
@@ -2,10 +2,8 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common AOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/T00F/device.mk)
@@ -15,7 +13,7 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/T00F/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_T00F
+PRODUCT_NAME := aosp_T00F
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_T00F
 PRODUCT_MANUFACTURER := asus
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo lineage_T00F-userdebug
-add_lunch_combo lineage_T00F-eng
+add_lunch_combo aosp_T00F-userdebug
+add_lunch_combo aosp_T00F-eng
