1b5773855219372361ab7f82cc8a706d788803c8
Artem Borisov <dedsa2002@gmail.com>
2017-08-23 19:32:56 +0300

    T00F: init: logging fix and cleanup

--- init/init_ctp.cpp
+++ init/init_ctp.cpp
@@ -49,7 +49,7 @@ static int read_file2(const char *fname, char *data, int max_size)
 
     fd = open(fname, O_RDONLY);
     if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
+        LOG(ERROR) << "failed to open '" << fname << "'\n";
         return 0;
     }
 
@@ -85,30 +85,7 @@ static void get_serial()
     }
 }
 
-static void configure_zram() {
-    char buf[128];
-    FILE *f;
-
-    if ((f = fopen(MEMINFO_FILE, "r")) == NULL) {
-        ERROR("%s: Failed to open %s\n", __func__, MEMINFO_FILE);
-        return;
-    }
-
-    while (fgets(buf, sizeof(buf), f) != NULL) {
-        if (strncmp(buf, MEMINFO_KEY, strlen(MEMINFO_KEY)) == 0) {
-            int mem = atoi(&buf[strlen(MEMINFO_KEY)]);
-            const char *mode = mem < ZRAM_MEM_THRESHOLD ? "true" : "false";
-            INFO("%s: Found total memory to be %d kb, zram enabled: %s\n", __func__, mem, mode);
-            property_set(ZRAM_PROP, mode);
-            break;
-        }
-    }
-
-    fclose(f);
-}
-
 void vendor_load_properties()
 {
     get_serial();
-    configure_zram();
 }
